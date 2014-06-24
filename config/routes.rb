12107dffc22dcc8270c1e9bd65bbaf3f83a679d0
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-06-24 14:42:07 -0700

    added profile image css

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'homepage'
+  get '/about' => 'high_voltage/pages#show', id: 'about'
+
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   resources :activities
-
-
-
-
 
 end
