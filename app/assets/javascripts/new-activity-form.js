d89ee2f65e4895af80031251f91af1f2056ca586
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-06-29 21:04:49 -0700

    New activity page styling progressed

---
+++ app/assets/javascripts/new-activity-form.js
@@ -0,0 +1,7 @@
+$(".datepicker").datepicker();
+
+$(".datepicker").on("change", function () {
+    var id = $(this).attr("id");
+    var val = $("label[for='" + id + "']").text();
+    $("#msg").text(val + " changed");
+});
