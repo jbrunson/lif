d8b73d17c47d712775fcf1f4a4155b99e940bdaa
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-07-03 12:42:35 -0700

    Adjusting users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @activities = @user.activities.current_and_future.limit(5)
-    @matches = @user.matches.order(created_at: :desc).limit(5)
+    @matches = @user.matches.order(created_at: :desc).limit(4)
     @photos = instagram_photos if @user.identities.any? { |identity| identity.provider == "instagram"  }
   end
 
