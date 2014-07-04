d16875d51145a7d82181302e994e6e8cd1c96c14
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-07-04 14:16:33 -0700

    updated users seeds

--- db/users_seed.rb
+++ db/users_seed.rb
@@ -6,3 +6,16 @@
   match_preference = ["male", "female", "any"].sample
   User.create(name: name, email: email, password: password, password_confirmation: password, gender: gender, match_preference: match_preference)
 end
+
+photos_of_women = [
+"http://s16.postimg.org/x4gu2l4ol/image.jpg",
+"http://s16.postimg.org/oq17ei3n9/image.jpg",
+"http://s16.postimg.org/nvensgvsl/image.jpg",
+"http://s16.postimg.org/5jo0bbn5h/image.jpg",
+"http://s16.postimg.org/yburusut1/image.jpg",
+"http://s16.postimg.org/yburusut1/image.jpg",
+"http://s16.postimg.org/oii5vqi2d/image.jpg",
+"http://s16.postimg.org/98iaojmk5/image.jpg",
+]
+
+vancouver_ip_address = "64.114.24.114"
