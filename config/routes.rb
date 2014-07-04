5fb557cc504d50c0fdb544cac6b4c1ba714aac80
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-07-03 17:23:10 -0700

    minor change

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   get "/pages/*id" => 'pages#show', as: :page, format: false
 
+  # match "/about" => 'pages#show', id: 'about', via[:get]
+
   root to: 'pages#show', id: 'homepage'
 
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   resources :matches do
