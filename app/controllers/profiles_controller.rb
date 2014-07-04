4f9e1379a864cc92e262f8e6501066a7e069a99b
Jeff Brunson <jeff.a.brunson@gmail.com>
2014-07-04 16:07:10 -0700

    removed elements from user show. add instagram on chat

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @photos = instagram_photos if @user.identities.any? { |identity| identity.provider == "instagram"  }
+    @photos = instagram_photos_for(@user) if @user.identities.any? { |identity| identity.provider == "instagram"  }
     if @user
       @match = match_exists?(current_user, @user)
       render action: :show
