c4240366216fb977d304e69923a32a9801033fcf
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-07-04 15:10:56 -0700

    added updated seed image links

--- db/users_seed.rb
+++ db/users_seed.rb
@@ -5,14 +5,14 @@
   gender = ["male", "female"].sample
   match_preference = ["male", "female", "any"].sample
   photos_of_women = [
-"http://s16.postimg.org/x4gu2l4ol/image.jpg",
-"http://s16.postimg.org/oq17ei3n9/image.jpg",
-"http://s16.postimg.org/nvensgvsl/image.jpg",
-"http://s16.postimg.org/5jo0bbn5h/image.jpg",
-"http://s16.postimg.org/yburusut1/image.jpg",
-"http://s16.postimg.org/yburusut1/image.jpg",
-"http://s16.postimg.org/oii5vqi2d/image.jpg",
-"http://s16.postimg.org/98iaojmk5/image.jpg",
+"http://s1.postimg.org/3mh6io64f/image.jpg",
+"http://s1.postimg.org/gugkbm3nj/image.jpg",
+"http://s1.postimg.org/3xymvfmkf/image.jpg",
+"http://s1.postimg.org/9kuzswp33/image.jpg",
+"http://s1.postimg.org/ldssx4lj3/image.jpg",
+"http://s1.postimg.org/yvzp9exof/image.jpg",
+"http://s1.postimg.org/jsdj5bbb3/image.jpg",
+"http://s1.postimg.org/xxjc74kcf/image.jpg"
 ]
 
 ip_addresses = ["64.114.24.114", "66.180.125.0"]
