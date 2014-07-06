2b819175edd9f1beb35d35d64752e0bc8aa11822
Jeff Brunson <jeff.a.brunson@gmail.com>
2014-07-06 13:51:53 -0700

    changed paths and added chat to profile page

--- app/assets/javascripts/new-activity-form.js
+++ app/assets/javascripts/new-activity-form.js
@@ -7,3 +7,12 @@ $(".datepicker").on("change", function () {
     var val = $("label[for='" + id + "']").text();
     $("#msg").text(val + " changed");
 });
+
+
+$(".dislike-user").on("click", function(){
+  $(this).closest(".col-md-3").addClass("hide");
+});
+
+$(".like-button").on("click", function(){
+  $(this).val("Liked");
+});
