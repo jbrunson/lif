3158186f8d79bdc9257bef0e5197df830d51bf00
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-07-06 21:40:18 -0700

    Fixed chat page height

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -68,3 +68,11 @@ $(function() {
         $('div#status').text('Pusher subscription_error');
       });
     });
+
+$(window).resize(function() {
+  var headerHeight = $('.navbar-default').height();
+  var chatUIHeight = $('.chat-interface').height();
+  $('.min-h-29').height($(window).height() - (headerHeight + chatUIHeight + 60));
+});
+
+$(window).trigger('resize');
